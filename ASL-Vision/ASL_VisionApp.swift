f1ab530175b8aaf392621a5c91bed64e644e730b
Karan <karan.lnu001@umb.edu>
2024-06-01 14:09:29 -0400

    Initial Commit

---
+++ ASL-Vision/ASL_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASL_VisionApp.swift
+//  ASL-Vision
+//
+//  Created by Karan . on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ASL_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASL-VisionUITests/ASL_VisionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ASL_VisionUITestsLaunchTests.swift
+//  ASL-VisionUITests
+//
+//  Created by Karan . on 6/1/24.
+//
+
+import XCTest
+
+final class ASL_VisionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
